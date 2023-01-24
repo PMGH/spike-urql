d8a49e2a68a3ea8b09137598666ecbb7871ce8f6
Peter McCready <peter.mccready@ooni.com>
2023-01-24 11:38:49 +0000

    WIP - use codegen for typescript typings

--- urql/queries.ts
+++ urql/queries.ts
@@ -1,21 +1,22 @@
 import { gql } from "urql";
+import { graphql } from '../src/gql'
 
-export const GET_COUNTRIES_QUERY = gql`
-  query {
+export const GET_COUNTRIES_QUERY = graphql(`
+  query GET_COUNTRIES_QUERY {
     countries {
       code
       name
       emoji
     }
   }
-`;
+`);
 
-export const GET_COUNTRY_BY_ID_QUERY = gql`
-  query($code: ID!) {
+export const GET_COUNTRY_BY_ID_QUERY = graphql(`
+  query GET_COUNTRY_BY_ID_QUERY($code: ID!) {
     country(code: $code) {
       code
       name
       emoji
     }
   }
-`;
+`);
